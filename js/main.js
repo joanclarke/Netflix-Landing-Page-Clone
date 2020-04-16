fe1f47de10efdf3266bb759f9495d51003cd3ac1
Joan Clarke <joanclarke100@gmail.com>
2020-04-16 15:22:39 -0500

    make correction to naming error - change tab-2-content-item to tab-content-item -- line 61

--- js/main.js
+++ js/main.js
@@ -1,30 +1,3 @@
-// const tabItems = document.querySelectorAll('.tab-item');
-// const tabContentItems = document.querySelectorAll('.tab-content-item');
-
-// //Select tab function item
-
-// function selectItem(e) {
-// 	removeBorder();
-// 	removeShow();
-// 	// Add border to current tab
-// 	this.classList.add('tab-border');
-// 	// Grab content item from the dom
-// 	const tabContentItem = document.querySelector(`#${this.id}-content`);
-// 	// Add show class
-// 	tabContentItem.classList.add('show');
-// }
-
-// function removeBorder() {
-// 	tabItems.forEach((item) => item.classList.remove('tab-border'));
-// }
-
-// function removeShow() {
-// 	tabContentItems.forEach((item) => item.classList.remove('show'));
-// }
-
-// // Listen for click
-// tabItems.forEach((item) => item.addEventListener('click', selectItem));
-
 const tabItems = document.querySelectorAll('.tab-item');
 const tabContentItems = document.querySelectorAll('.tab-content-item');
 
